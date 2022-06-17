61178ad21be012695bc3bd9991c46873954a7562
Marshal <Marshalgcom@gmail.com>
2022-06-17 07:58:28 +0530

    ...

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_jh/constants.dart';
 import 'package:project_jh/models/product.dart';
 import 'package:project_jh/screens/details/components/Product_Title_With_Image.dart';
 
@@ -19,6 +20,10 @@ class Body extends StatelessWidget {
               children: [
                 Container(
                   margin: EdgeInsets.only(top: size.height * 0.3),
+                  padding: EdgeInsets.only(
+                      top: size.height * 0.12,
+                      left: kDefaultPaddin,
+                      right: kDefaultPaddin),
                   //height: 6000,
                   decoration: const BoxDecoration(
                     color: Colors.white,
@@ -27,7 +32,47 @@ class Body extends StatelessWidget {
                       topRight: Radius.circular(24),
                     ),
                   ),
-                  child: Column(),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Column(
+                            children: [
+                              const Text("Color"),
+                              Row(
+                                children: const [
+                                  ColorDot(
+                                      color: Color(0xFF356C95),
+                                      isSelected: true),
+                                  ColorDot(color: Color(0xFF356C95)),
+                                  ColorDot(color: Color(0xFF356C95)),
+                                ],
+                              ),
+                            ],
+                          ),
+                          RichText(
+                            text: TextSpan(
+                              style: TextStyle(color: kTextColor),
+                              children: [
+                                const TextSpan(
+                                  text: "Quantity\n",
+                                ),
+                                TextSpan(
+                                  text: "${product.size} KG",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline5
+                                      ?.copyWith(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
                 ProductTitleWithImage(product: product),
               ],
@@ -35,6 +80,39 @@ class Body extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class ColorDot extends StatelessWidget {
+  final Color color;
+  final bool isSelected;
+
+  const ColorDot({
+    Key? key,
+    required this.color,
+    this.isSelected = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+          top: kDefaultPaddin / 4, right: kDefaultPaddin / 2),
+      padding: const EdgeInsets.all(2.5),
+      height: 24,
+      width: 24,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: isSelected ? color : Colors.transparent,
+        ),
+      ),
+      child: DecoratedBox(
+          decoration: BoxDecoration(
+        color: color,
+        shape: BoxShape.circle,
+      )),
     );
   }
 }
